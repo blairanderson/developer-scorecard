b6f2443197237a2b086bdace8c12a36a31a60a7b
blair anderson <blair81@gmail.com>
2014-02-01 16:17:05 -0700

    update outlets for routes

--- app/assets/javascripts/routes/user_route.js
+++ app/assets/javascripts/routes/user_route.js
@@ -7,17 +7,17 @@ ReportCard.UserRoute = Ember.Route.extend({
     this.controllerFor('user').set('model', model.user);
     stat = model.user.stats[0]
     if (stat.type === 'osrc') {
-      this.controllerFor('osrc').set('model', stat);
+      this.controllerFor('osrc').set('model', stat.event);
     }
     this.controllerFor('meta').set('model', model.meta_object);
   },
   renderTemplate: function() {
     this.render('osrc', {       // the template to render
-      outlet: 'right-sidebar',  // the name of the outlet in the route's template
+      outlet: 'main',
       controller: 'osrc'        // the controller to use for the template
     });
     this.render('user', {
-      outlet: 'main',
+      outlet: 'right-sidebar',  // the name of the outlet in the route's template
       controller: 'user'
     });
     this.render('javascript', {
