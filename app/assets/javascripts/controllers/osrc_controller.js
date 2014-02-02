f09f7aad1c8c0cbccea497029f83650b0a5860de
blair anderson <blair81@gmail.com>
2014-02-01 17:06:33 -0700

    add momentjs and some moment helpers

--- app/assets/javascripts/controllers/osrc_controller.js
+++ app/assets/javascripts/controllers/osrc_controller.js
@@ -1,2 +1,51 @@
 ReportCard.OsrcController = Ember.ObjectController.extend({
+  userLeader: function() {
+    var list, name, usage, result;
+    usage = this.get("model.usage");
+    list = usage.languages;
+
+    result = this.languages()
+
+    if (list[0].quantile < 50) {
+      result += this.quantile()
+    }
+    result += this.events();
+
+    result += "."
+    return result;
+  }.property("model.userLeader"),
+
+  languages: function() {
+    var name, usage, list, languages, result;
+    name = this.get("model.name");
+    usage = this.get("model.usage");
+    list = usage.languages;
+    result = '';
+
+    if (list && list.length > 0) {
+      languages = "" + list[0].language + ", " + list[1].language + ", and " + list[2].language;
+    }
+    result = "" + name + " is a " + languages +" hacker";
+    return result
+  },
+
+  quantile: function() {
+    var usage, list, result;
+    usage = this.get("model.usage");
+    list = usage.languages;
+    result = '';
+    result += "(one of the " + list[0].quantile + "% most active " + list[0].language + " users, ";
+    result += "and one of the " + list[1].quantile + "% most active " + list[1].language + " users) ";
+    
+    return result    
+  },
+
+  events: function() {
+    var result, usage, main, verbs;
+    usage = this.get("model.usage");
+    main = usage.events[0].type;
+    verbs = window.ReportCard.event_verbs
+    return "spends a lot of time " + verbs[main];
+  }
+
 });
--- app/assets/javascripts/helpers.js.coffee
+++ app/assets/javascripts/helpers.js.coffee
@@ -1,3 +1,9 @@
+Ember.Handlebars.registerBoundHelper 'currentDate', (option) ->
+  moment().format('LL');
+
+Ember.Handlebars.registerBoundHelper 'currentTime', (option) ->
+  moment().format('h:mm:ss a');
+
 Ember.Handlebars.helper 'link_to', (name, route) ->
   new Handlebars.SafeString("<a href='#{route}'>#{name}</a>")
 
